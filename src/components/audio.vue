485675d6e8593d619065b994738ef3d175cd3205
weizhengda <2625785357@qq.com>
2017-11-20 11:36:51 +0800

    重置项目

---
+++ src/components/audio.vue
@@ -0,0 +1,20 @@
+<template>
+   <div id="audio">
+        {{audio}}
+   </div>
+</template>
+<script>
+export default {
+  name: 'audio',
+  data() {
+      return {
+          audio: 'audio'
+      }
+  }
+}
+</script>
+<style scoped>
+
+</style>
+
+
